918296e5a57ac2fb0629c2354b1ddf36b4535e44
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-29 16:10:59 -0400

    workout type

--- models/workout.js
+++ models/workout.js
@@ -1,58 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  {
-    day: {
-      // For the schema of this attribute, define type and default
-      type: Date,
-      default: () => new Date(),
-    },
-    exercises: [
-      // Schema for each item in exercises array
-      {
-        type: {
-          // TODO: For the schema of this attribute, define type and trim and required
-          type: String,
-          trim: true,
-          required: "Please choose your exercise"         
-        },
-        name: {
-          // TODO: For the schema of this attribute, define type and trim and required
-          type: String,
-          trim: true,
-          required: "Please choose your exercise"
-         
-        },
-        duration: {
-          // TODO: For the schema of this attribute, define type as number and its required
-      
-        
-        },
-        weight: {
-          // TODO: For the schema of this attribute, define type as number
-         
-          
-        },
-        reps: {
-          // TODO: For the schema of this attribute, define type as number
-         
-        },
-        sets: {
-          // TODO: For the schema of this attribute, define type as number
-      
-        },
-        distance: {
-          // TODO: For the schema of this attribute, define type as number
-          
-        },
-      },
-    ],
+const workoutSchema = new Schema({
+  day: {
+    // For the schema of this attribute, define type and default
+    type: Date,
+    default: () => new Date(),
   },
+  exercises: [
+    // Schema for each item in exercises array
+    {
+      type: {
+        // TODO: For the schema of this attribute, define type and trim and required
+        type: String,
+        trim: true,
+        required: "Please choose your exercise",
+      },
+      name: {
+        // TODO: For the schema of this attribute, define type and trim and required
+        type: String,
+        trim: true,
+        required: "Please choose your exercise",
+      },
+      duration: {
+        // TODO: For the schema of this attribute, define type as number and its required
+        type: Number,
+        required: "Please enter a time",
+      },
+      weight: {
+        // TODO: For the schema of this attribute, define type as number
+        type: Number,
+      },
+      reps: {
+        // TODO: For the schema of this attribute, define type as number
+        type: Number,
+      },
+      sets: {
+        // TODO: For the schema of this attribute, define type as number
+        type: Number,
+      },
+      distance: {
+        // TODO: For the schema of this attribute, define type as number
+        type: Number,
+      },
+    },
+  ],
+});
 
-);
-
-const Workout = mongoose.model('Workout', workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
